654ea6f49f0d0c546e6c429744ccc50a3e20e1d9
Ashish jaswal <ashish.jaswal@one7sports.com>
2025-08-27 11:11:44 +0530

    redploying for env var

--- backend/index.js
+++ backend/index.js
@@ -3,15 +3,27 @@ const cors = require("cors");
 const app = express();
 const stockRoutes = require("./routes/stockRoutes");
 
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
 // for allow origin policy.
 app.use(cors());
 app.use(express.json());
 
-//api endpoint to 
+//api endpoint to check stocks data
 app.use("/api/v1/stocks", stockRoutes);
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
-const PORT = 5001;
+
+//health check of the server
+app.get("/", (req, res) => {
+    res.status(200).send(
+      `<div><p><h2>App Server is working fine on port ${process.env.PORT || PORT}</h2><p></div>`
+    );
+  });
+  
+
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
